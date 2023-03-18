c5e19a9bc0d95d76f86dff667b16000c1986cfb4
Vladimir Sankin <vladimir.niks36@gmail.com>
2023-03-18 21:52:20 +0300

    HomeWork_1

--- hw_1.6.py
+++ hw_1.6.py
@@ -7,6 +7,6 @@ if profit > 0:
     print('Прибыль')
     profitability = profit / income
     print(f'Рентабельность выручки {profitability:.2f}')
-    staffCount = int(input('Введите количество сотрдников фирмы: '))
-    profitByPerson = profit / staffCount
+    staffcount = int(input('Введите количество сотрдников фирмы: '))
+    profitbyperson = profit / staffCount
     print(f'Прибыль в расчете на одного сотрудника {profitByPerson:.2f}')
